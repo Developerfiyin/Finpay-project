4cae96b1960f4145764be28caa6f79adf268cf30
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-19 22:02:18 +0100

    Added a tag to the header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-
+   <a href="#">Products</a>
+  <a href="#">Customers</a>
+  <a href="#">Pricing</a>
+  <a href="#">Learn</a>
+ <a href="#"></a>
     </div>
   )
 }
