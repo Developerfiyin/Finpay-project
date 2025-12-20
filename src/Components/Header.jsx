ee5bae8e8015bd109804e419c3f41ec95438c8c7
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-20 23:35:29 +0100

    Styled button with tailwind cssj

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=' bg-[#8d603f] py-4 px-2 text-white'> 
-    <div className='flex justify-around'>
+    <div className=' bg-[#8d603f] py-4 px-2'> 
+    <div className='flex justify-around text-xl font-medium'>
    <h3 className='text-[#1A0A00] text-2xl font-medium'>Coffee Tea</h3>
-    <div className='flex justify-center space-x-4'>
+    <div className='flex justify-center space-x-4 text-[#36231D]  '>
    <a href="#home">Home</a>
    <a href="#menu">Menu</a>
    <a href="#contact">Contact</a>
@@ -13,7 +13,7 @@ const Header = () => {
    <a href="#coconut">Coconut</a>
     </div> 
     
-    <button className='border-2 bg-[#36231D] m-2 p-2 rounded-3xl text-base font-medium'>Our Coffee Shop</button>
+    <button className='border-2 text-[#8d603f] bg-[#36231D] m-2 p-2 rounded-3xl text-base font-medium hover:bg-[#8d603f] hover:text-[#36231D] ease-in-out duration-300 '>Our Coffee Shop</button>
 
     </div>
      </div>
