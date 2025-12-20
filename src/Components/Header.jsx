85ff736932df41335894e33e08afbeaba7e4fdaf
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-20 23:22:23 +0100

    Updated header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=' bg-[#DABC9A]'> 
-
-   <img src="" alt="" /> 070 344 69 731 70 65 27 38 99
-   <h3 className='text-[#1A0A00] text-2xl font-medium'>Coffee</h3>
+    <div className=' bg-[#8d603f] py-4 px-2 text-white'> 
     <div className='flex justify-around'>
+   <h3 className='text-[#1A0A00] text-2xl font-medium'>Coffee Tea</h3>
+    <div className='flex justify-center space-x-4'>
    <a href="#home">Home</a>
    <a href="#menu">Menu</a>
    <a href="#contact">Contact</a>
    <a href="#leoting">Leoting</a>
-    </div>
+   <a href="#coconut">Coconut</a>
+    </div> 
     
-    <button className='border-2 border-teal-500 m-2 p-1 rounded-lg text-base font-medium '>Login </button>
-    <button className='bg-teal-600'>Sign up</button>
+    <button className='border-2 bg-[#36231D] m-2 p-2 rounded-3xl text-base font-medium'>Our Coffee Shop</button>
 
     </div>
+     </div>
   )
 }
 
