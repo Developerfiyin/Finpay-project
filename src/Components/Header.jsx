3c998aed4cd2c0f9b24090fd85ad229e86de2d21
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-21 20:56:17 +0100

    Updated header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
     <div className='flex justify-center space-x-4 text-[#36231D]  '>
    <a href="#home">Home</a>
    <a href="#menu">Menu</a>
-   <a href="#contact">Contact</a>
-   <a href="#leoting">Leoting</a>
-   <a href="#coconut">Coconut</a>
+   <a href="#about">About us</a>
+   <a href="#newsletter">Newsletter</a>
+   <a href="#footer">Footer</a>
     </div> 
     
     <button className='border-2 text-[#8d603f] bg-[#36231D] m-2 p-2 rounded-3xl text-base font-medium hover:bg-[#8d603f] hover:text-[#36231D] ease-in-out duration-300 '>Our Coffee Shop</button>
