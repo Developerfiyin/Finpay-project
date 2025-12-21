c6bc23966f919c4485d37f13975f68d1c516199e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-21 23:58:14 +0100

    Use a coffe image for the background in hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import image  from '../assets/hero coffee.png'
 
 const Hero = () => {
   return (
-    <div className=''> 
+    <section className='min-h-screen flex justify-center text-center bg-cover bg-center ' style={{backgroundImage : url`(${image})`}}> 
        <h3>Brewed to Perfection</h3>
-       
-    </div>
+
+    </section>
   )
 }
 
