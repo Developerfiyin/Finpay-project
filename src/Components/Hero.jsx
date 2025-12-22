b8686dcc7b1c76bdd7af1632c2ec662fb7082f26
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-22 09:49:47 +0100

    styled h2 tag

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,8 +3,8 @@ import image  from '../assets/hero coffee.png'
 
 const Hero = () => {
   return (
-    <section className='min-h-screen flex justify-center text-center bg-cover bg-center ' style={{backgroundImage : url`(${image})`}}> 
-       <h3>Brewed to Perfection</h3>
+    <section className='min-h-screen flex items-center justify-center text-center bg-cover bg-center '   style={{ backgroundImage: `url(${image})` }}> 
+       <h2 className='text-9xl text-[#36231D] font-medium '>Brewed to Perfection</h2>
 
     </section>
   )
