a48ccf2488e1c6afa54a435dae4170ab535c7d3e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-23 23:57:59 +0100

    Added a div containig cappuccino content

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -8,7 +8,18 @@ const Menu = () => {
        <h3 className='text-xl font-bold text-center '>Our Menu </h3> 
      <h1 className='text-7xl font-semibold mb-4'>Discover Our <br /> Exquiste Menu</h1>
 
-            <div className='grid grid-cols-3 gap-5'>
+            <div className='flex  gap-5'>
+                <div className='py-4 bg-white px-4 rounded-lg'>
+            <div>
+             <img src={image} alt="Cappucino coffee image" className='w-60 rounded-lg' />
+             </div>
+
+             <div className='flex justify-between mt-4'>
+              <h4> Cappuccino</h4>
+              <h6> $12.50</h6>
+             </div>
+
+                </div>
             </div>
 
         </section>
