6902389dd235f811699ba67782e0e510d954629e
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-23 16:24:05 +0100

    Made an error

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -4,19 +4,11 @@ import image  from '../assets/cappuccino.png'
 
 const Menu = () => {
   return (
-    <section className='py-4 px-4 text-[#36231D] bg-white'>
+    <section className='py-8 px-4 text-[#36231D] bg-whte'>
        <h3 className='text-xl font-bold text-center '>Our Menu </h3> 
      <h1 className='text-7xl font-semibold mb-4'>Discover Our <br /> Exquiste Menu</h1>
 
-            <div className='flex justify-around mt-4 '>
-              <div className='bg-white m-4 border-4 border-[#36231D] '>
-                <div>
-
-              
-                <img src={image} alt="Cappuccino coffee image" className='w-25 ' />
-                  </div>
-              </div>
-
+            <div className='grid grid-cols-3 gap-5'>
             </div>
 
         </section>
