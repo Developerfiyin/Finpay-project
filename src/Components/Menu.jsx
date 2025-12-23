c0344ccdfddf8f7985633ff181d8825f418fb2c5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-24 00:53:44 +0100

    updated menu.jsx

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -4,22 +4,20 @@ import image  from '../assets/cappuccino.png'
 
 const Menu = () => {
   return (
-    <section className='py-8 px-4 text-[#36231D] bg-whte'>
+    <section className='py-8 px-4 text-[#36231D] bg-white'>
        <h3 className='text-xl font-bold text-center '>Our Menu </h3> 
      <h1 className='text-7xl font-semibold mb-4'>Discover Our <br /> Exquiste Menu</h1>
 
-            <div className='flex  gap-5'>
-                <div className='py-4 bg-white px-4 rounded-lg'>
+            <div className='flex gap-5'>
+            <div className='py-4 bg-white px-4 rounded-lg shadow-md shadow-[#36231d]'>
             <div>
              <img src={image} alt="Cappucino coffee image" className='w-60 rounded-lg' />
              </div>
-
              <div className='flex justify-between mt-4'>
-              <h4 className='text-[#36231d]'> Cappuccino</h4>
-              <h6 className='text-[#8d603f]'> $12.50</h6>
+              <h4 className='text-[#36231d] font-medium text-xl'> Cappuccino</h4>
+              <h6 className='text-[#8d603f] font-semibold'> $12.50</h6>
              </div>
-
-                </div>
+            </div>
             </div>
 
         </section>
