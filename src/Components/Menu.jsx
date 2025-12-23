370362d63f5c0f1e4270f0e66f22cc9bc1b9af85
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-23 06:40:08 +0100

    Styled the menu text

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import image  from '../assets/cappuccino.png'
+
 
 const Menu = () => {
   return (
     <section className='py-4 px-4 text-[#36231D] bg-white'>
        <h3 className='text-xl font-bold text-center '>Our Menu </h3> 
-        
-     <h1 className='text-7xl font-semibold'>Discover Our <br /> Exquiste Menu</h1>
+     <h1 className='text-7xl font-semibold mb-4'>Discover Our <br /> Exquiste Menu</h1>
 
+            <div className='flex justify-around mt-4 '>
+              <div className='bg-white m-4 border-4 border-[#36231D] '>
+                <div>
+
+              
+                <img src={image} alt="Cappuccino coffee image" className='w-25 ' />
+                  </div>
+              </div>
+
+            </div>
 
         </section>
   )
