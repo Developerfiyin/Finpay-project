5f8a69816a978754ce6665cd98d57e52c0195a8f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-24 16:48:26 +0100

    Updated menu.jsx

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -24,7 +24,7 @@ const Menu = () => {
 
              <div className='py-2 bg-white px-4 rounded-lg shadow-sm shadow-[#36231d]'>
             <div>
-             <img src={Ristretto} alt="Rist coffee image" className='w-60 h-60 rounded-lg' />
+             <img src={doppio} alt="Doppio coffee image" className='w-60 h-60 rounded-lg' />
              </div>
              <div className='flex justify-between mt-4'>
               <h4 className='text-[#36231d] font-medium text-xl'> Doppio</h4>
@@ -32,15 +32,15 @@ const Menu = () => {
              </div>
             </div>
 
-         {/*   <div className='py-2 bg-white px-4 rounded-lg shadow-sm shadow-[#36231d]'>
+            <div className='py-2 bg-white px-4 rounded-lg shadow-sm shadow-[#36231d]'>
             <div>
-             <img src={doppio} alt="Doppio coffee image" className='w-60 rounded-lg' />
+             <img src={Ristretto} alt="Ristretto coffee image" className='w-60 h-60 rounded-lg' />
              </div>
              <div className='flex justify-between mt-4'>
-              <h4 className='text-[#36231d] font-medium text-xl'> Doppio</h4>
-              <h6 className='text-[#8d603f] font-semibold'> $18.00</h6>
+              <h4 className='text-[#36231d] font-medium text-xl'>Ristretto</h4>
+              <h6 className='text-[#8d603f] font-semibold'> $14.75</h6>
              </div>
-            </div> */}
+            </div> 
 
 
             </div>
