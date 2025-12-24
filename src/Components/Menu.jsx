1aee0bf7604e0640527ffd6d4957e53c4909a51d
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-24 16:19:23 +0100

    Updated menu.jsx

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import image  from '../assets/cappuccino.png'
+import doppio from '../assets/Doppio.png'
 
 
 const Menu = () => {
@@ -9,7 +10,7 @@ const Menu = () => {
      <h1 className='text-7xl font-semibold mb-4'>Discover Our <br /> Exquiste Menu</h1>
 
             <div className='flex justify-evenly gap-5'>
-            <div className='py-4 bg-white px-4 rounded-lg shadow-sm shadow-[#36231d]'>
+            <div className='py-2 bg-white px-4 rounded-lg shadow-sm shadow-[#36231d]'>
             <div>
              <img src={image} alt="Cappucino coffee image" className='w-60 rounded-lg' />
              </div>
@@ -20,13 +21,13 @@ const Menu = () => {
             </div>
 
 
-             <div className='py-4 bg-white px-4 rounded-lg shadow-sm shadow-[#36231d]'>
+             <div className='py-2 bg-white px-4 rounded-lg shadow-sm shadow-[#36231d]'>
             <div>
-             <img src={image} alt="Cappucino coffee image" className='w-60 rounded-lg' />
+             <img src={doppio} alt="Doppio coffee image" className='w-60 rounded-lg' />
              </div>
              <div className='flex justify-between mt-4'>
-              <h4 className='text-[#36231d] font-medium text-xl'> Cappuccino</h4>
-              <h6 className='text-[#8d603f] font-semibold'> $12.50</h6>
+              <h4 className='text-[#36231d] font-medium text-xl'> Doppio</h4>
+              <h6 className='text-[#8d603f] font-semibold'> $18.00</h6>
              </div>
             </div>
             </div>
