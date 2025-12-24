f550e200f630955e60a6db677e57371a5e131f0f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-25 00:02:20 +0100

    Added another card for coffee content

--- src/Components/Speciality.jsx
+++ src/Components/Speciality.jsx
@@ -6,6 +6,7 @@ const Speciality = () => {
     <main className='py-4 px-4 bg-[#36231d] text-white text-center items-center '>
           <h5 className='mt-6'> Our Speciality</h5>
           <h1 className='text-6xl font-semibold'>We Deliver Good Vibes everyday</h1>
+
            <div className='flex justify-evenly mt-6'>
             <div className=''>
             <img src={coffee} alt="The exceptional coffee image" className='rounded-t-lg'/>
@@ -13,6 +14,15 @@ const Speciality = () => {
                     <h1 className='text-5xl p-7 font-semibold text-[#36231d]'> Exceptional Coffee</h1>
                  </div>
             </div>
+            
+             <div className=''>
+            <img src={coffee} alt="The exceptional coffee image" className='rounded-t-lg'/>
+                 <div className='bg-white rounded-b-lg'> 
+                    <h1 className='text-5xl p-7 font-semibold text-[#36231d]'> Exceptional Coffee</h1>
+                 </div>
+            </div>
+            
+
            </div>
         </main>
   )
