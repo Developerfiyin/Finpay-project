2dd2ef5a9e452cb200561a38483aa3a4363caf52
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-24 23:38:06 +0100

    Added an image in a div

--- src/Components/Speciality.jsx
+++ src/Components/Speciality.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import coffee from '../assets/coffee.png'
 
 const Speciality = () => {
   return (
     <main className='py-4 px-4 bg-[#36231d] text-white text-center items-center '>
           <h5 className='mt-6'> Our Speciality</h5>
-          <h1 className='text-6xl font'>We Deliver Good Vibes everyday</h1>
-        
+          <h1 className='text-6xl font-semibold'>We Deliver Good Vibes everyday</h1>
+           <div className='flex justify-evenly'>
+            <div>
+            <img src={coffee} alt="The exceptional coffee image " />
+
+            </div>
+           </div>
         </main>
   )
 }
