c4abc792816e24531962010a9a381e51ede0f4c6
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-24 23:52:06 +0100

    updated section

--- src/Components/Speciality.jsx
+++ src/Components/Speciality.jsx
@@ -6,10 +6,12 @@ const Speciality = () => {
     <main className='py-4 px-4 bg-[#36231d] text-white text-center items-center '>
           <h5 className='mt-6'> Our Speciality</h5>
           <h1 className='text-6xl font-semibold'>We Deliver Good Vibes everyday</h1>
-           <div className='flex justify-evenly'>
+           <div className='flex justify-evenly mt-6'>
             <div>
             <img src={coffee} alt="The exceptional coffee image " />
-
+                 <div className='bg-white'> 
+                    <h1 className='text-5xl p-5 font-semibold text-[#36231d]'> Exceptional Coffee</h1>
+                 </div>
             </div>
            </div>
         </main>
