ec97c59ea7af84609cb3f50769dce1cbaebcd5fb
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-26 00:00:07 +0100

    updated section

--- src/Components/Speciality.jsx
+++ src/Components/Speciality.jsx
@@ -10,22 +10,22 @@ const Speciality = () => {
           <h1 className='text-6xl font-semibold'>We Deliver Good Vibes everyday</h1>
 
            <div className='grid grid-cols-1 sm:grid-cols-3 gap-5'>
-            <div className='w-full h-64'>
+            <div className=''>
             <img src={coffee} alt="The exceptional coffee image" className='rounded-t-lg'/>
-                 <div className='bg-white rounded-b-lg w-full h-full object-cover'> 
+                 <div className='bg-white rounded-b-lg'> 
                     <h1 className='text-5xl p-7 font-semibold text-[#36231d]'> Exceptional Coffee</h1>
                  </div>
             </div>
             
              <div className=''>
-            <img src={Worker} alt="The exceptional coffee image" className='rounded-t-lg min-w-auto'/>
+            <img src={Worker} alt="The exceptional coffee image" className='rounded-t-lg'/>
                  <div className='bg-white rounded-b-lg'> 
                     <h1 className='text-5xl p-7 font-semibold text-[#36231d]'>Flexible Co-working</h1>
                  </div>
-            </div>
+            </div> 
 
-      <div className=''>
-            <img src={Worker1} alt="The exceptional coffee image" className='rounded-t-lg h-60 w-60'/>
+           <div className=''>
+            <img src={Worker1} alt="The exceptional coffee image" className='rounded-t-lgw-80 '/>
                  <div className='bg-white rounded-b-lg'> 
                     <h1 className='text-5xl p-7 font-semibold text-[#36231d]'> Exceptional Coffee</h1>
                  </div>
