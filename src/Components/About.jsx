d647a7b67e317b1e9b6d0eba13c548618ae199bf
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-28 22:18:27 +0100

    updated about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,12 +8,12 @@ const About = () => {
   <div className='flex justify-evenly gap-3'>
 
   <div className='flex flex-col'> 
-    <p className='text-2xl font-semibold'>About Us</p>
-    <h1 className='text-5xl font-semibold '>This Is All About Our Coffee Shop</h1>
+    <p className='text-2xl font-semibold mb-4 '>About Us</p>
+    <h1 className='text-5xl font-semibold mb-4 '>This Is All About Our Coffee Shop</h1>
    <h5> NEAT Coffee Bar aims to create a flexible co-working environment <br />
     that provides solution to a wide variety of working professionals. <br />
-    The best place to do business with best coffe of the course. Neat <br />
-    
+    The best place to do business with best coffee of the course! Neat <br />
+    Coffee Bar is located directly in historic Downtown New port richey.
    </h5>
 
 
