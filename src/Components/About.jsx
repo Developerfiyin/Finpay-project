fc00f59ead1a1d313d7d6be50524653122b0e186
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-28 12:09:58 +0100

    Updated About.jsx section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
+import image from '../assets/Coffee shop.png'
 
 const About = () => {
   return (
-    <main className='py-6 px-4 '>
+    <main className='py-6 px-4 mx-4 pl-5 text-[#80603f]'>
           
-<div className='flex justify-around'>
+  <div className='flex justify-evenly gap-3'>
 
-<div className='flex flex-col'> 
+  <div className='flex flex-col'> 
+    <p className='text-2xl font-semibold'>About Us</p>
+    <h1 className='text-5xl font-semibold '>This Is All About Our Coffee Shop</h1>
+   <h5> NEAT Coffee Bar aims to create a flexible co-working environment <br />
+    that provides solution to a wide variety of working professionals. <br />
+    The best place to do business with best coffe of the course. Neat <br />
+    
+   </h5>
+
+
+ </div>
+
+ <div>
+    <img src={image} alt="" className='rounded-3xl w-150 h-150
+     ' />
+ </div>
 
 </div>
-
-</div>
-
+         
     </main>
   )
 }
