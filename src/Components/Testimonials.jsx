7dde2b046cb927b24d9ad6f48f4ab6659d1f46de
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-29 09:45:40 +0100

    Added a div for the coffe container

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <main>
+    <main className='py-4 px-4 mt-2 bg-[#36231d] text-[#80603f] text-center items-center'>
+      <h5 className='text-2xl font-medium'>TESTIMONIALS</h5>
+      <h2 className='text-7xl font-semibold '>WHAT OUR CLIENTS SAYS</h2>
+      <div className='mt-4'>
+     <div className='flex gap-5 '> 
+      <div>
+
+      </div>
+     
+
+     </div>
+
+      </div>
 
 
-
-
-
-        Testimonials</main>
+    </main>
   )
 }
 
