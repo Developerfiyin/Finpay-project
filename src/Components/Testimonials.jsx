7be02b3e23169ff3a55f908325f40121f4badc33
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-29 10:50:48 +0100

    Updated testimonials.jsx

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -9,11 +9,11 @@ const Testimonials = () => {
     <main className='py-4 px-4  bg-white text-[#80603f] text-center items-center'>
       <h5 className='text-2xl font-medium'>TESTIMONIALS</h5>
       <h2 className='text-7xl font-semibold '>WHAT OUR CLIENTS SAYS</h2>
-      <div className='mt-4'>
-     <div className='flex gap-5 grid-cols-3 grid '> 
-      <div className='m-4 p-4 border-2 rounded-2xl hover:bg-[#8d603f] hover:text-[#36231d] ease-in-out duration-300 '>
+    
+     <div className='flex gap-5 grid-cols-3 grid mt-4'> 
+      <div className='m-4 p-4 border-2 rounded-2xl shadow-sm shadow-[#36231d] ease-in-out duration-300 '>
          <div className='flex gap-4 mb-4'> {/* THE IMAGE AND  NAME DIV */ }
-            <img src={image} alt="The member image " className='rounded-full h-15 w-15' /> 
+            <img src={image} alt="The member image" className='rounded-full h-15 w-15' /> 
             <div> 
            <h4 className='text-3xl font-semibold'> Joanna Julie </h4>
             <h5 className='text-xl font-medium '>Founder Main Street Bakery</h5>
@@ -22,10 +22,26 @@ const Testimonials = () => {
           <h3>NEAT has completely transform how i work. The coffee is amazing, the space is inspiring and the vibe is always welcoming! </h3>
       </div>
      
+       <div className='m-4 p-4 border-2 rounded-2xl shadow-sm shadow-[#36231d] ease-in-out duration-300 '>
+         <div className='flex gap-4 mb-4'> {/* THE IMAGE AND  NAME DIV */ }
+            <img src={image1} alt="The second member image " className='rounded-full h-15 w-15' /> 
+            <div> 
+           <h4 className='text-3xl font-semibold'> John Doe </h4>
+            <h5 className='text-xl font-medium '>Founder .32degreebelowfla</h5>
+            </div>
+         </div>
+          <h3>NEAT has completely transform how i work. The coffee is amazing,
+             the space is inspiring and the vibe is always welcoming! </h3>
+      </div>
+     
+
+
+
+
 
      </div>
 
-      </div>
+      
 
 
     </main>
