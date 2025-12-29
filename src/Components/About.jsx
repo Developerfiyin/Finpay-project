644de2e88f34fc8b5bc8fdf3d9d43e1e74eab215
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-29 04:09:56 +0100

    Added button and styled it

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import image from '../assets/Coffee shop.png'
 
 const About = () => {
   return (
-    <main className='py-6 px-4 mx-4 pl-5 text-[#80603f]'>
+    <main className='py-6 px-4 pl-5 text-[#80603f] bg-[#36231d]'>
           
   <div className='flex justify-evenly gap-3'>
 
@@ -15,12 +15,14 @@ const About = () => {
     The best place to do business with best coffee of the course! Neat <br />
     Coffee Bar is located directly in historic Downtown New port richey.
    </h5>
-
+   <br />
 <h5>Neat Coffee Bar aims a flexible co-working environment <br />
  that provides solution to a wide variety of working professionals <br />
  The best place to do business with the best coffee of course ! Neat <br />
  Coffee Bar is located directly in historic Downtown New port richey.
   </h5>
+    <button className='border-2 w-50 text-[#8d603f] bg-[#36231D] m-2 p-2 rounded-3xl text-base font-medium hover:bg-[#8d603f] hover:text-[#36231D] ease-in-out duration-300 '>Read More</button>
+
 
  </div>
 
