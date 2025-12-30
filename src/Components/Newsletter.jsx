d52045cf593936b6904e01933175c09c04f6b61a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-30 06:58:03 +0100

    Added button tag

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -10,7 +10,7 @@ const Newsletter = () => {
         <input type="text" placeholder='Enter your email'
          className='pl-3 border-none outline-none px-3 mt-7 ring-2 ring-[#36231d] pr-3 py-2 mr-4 items-center rounded-full bg-white w-70 h-15'  
          />
-            <button className='bg-[#36231d] rounded-3xl w-50 h-10 '>Subscribe</button>
+         <button className='bg-[#36231d] ease-in-out duration-500 font-semibold hover:text-white hover:bg-[#80603f] rounded-3xl w-50 h-10 text-[#80603f] '>Subscribe</button>
     </main>
   )
 
