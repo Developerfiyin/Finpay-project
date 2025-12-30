47549b59cd197fc11bd0bf545a47230ea04de796
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-30 13:04:14 +0100

    Deleted unnecessary lines of code

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -3,7 +3,6 @@ import image from '../assets/member 1.png'
 import image1 from '../assets/member 2.png'
 import image2 from '../assets/member 3.png'
 
-
 const Testimonials = () => {
   return (
     <main className='py-4 px-4  bg-white text-[#80603f] text-center items-center'>
@@ -40,19 +39,15 @@ const Testimonials = () => {
             <img src={image2} alt="The member image " className='rounded-full h-15 w-15' /> 
             <div> 
            <h4 className='text-3xl font-semibold'>John Smith </h4>
-            <h5 className='text-xl font-medium '>Founder Main Street Bakery</h5>
+            <h5 className='text-xl font-medium'>Founder Main Street Bakery</h5>
             </div>
          </div>
           <h3>NEAT has completely transform how i work. The coffee is amazing, the space is inspiring and the vibe is always welcoming! </h3>
       </div>
      </div>
      
-  import { FaBeer } from 'react-icons/fa';
 
-  class Question extends React.Component {
-   <h3> Lets go for a <FaBeer />? </h3>
-  }
-
+  
          <div className='items-center space-x-5 mt-7'>
             <button className='rounded-full h-12 w-12 border-2 border-black'></button>
             <button className='rounded-full h-12 w-12 border-none bg-black'></button>
